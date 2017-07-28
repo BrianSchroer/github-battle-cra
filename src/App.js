5b0c12c959d8fe3939e859a25a5c3ac3327c9275
Brian Schroer <brianschroer@gmail.com>
2017-07-28 13:52:31 -0500

    Initialize with create-react-app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <h2>Github Battle</h2>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
